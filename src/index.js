827b7a0d065168d2cf5e434fdfb3361d567787a9
Valencia Cheung <153874853+ValenciaCheung@users.noreply.github.com>
2025-01-04 16:52:29 -0800

    add ai api and, change css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "乱花渐欲迷人眼,浅草才能没马蹄",
+    strings: response.data.answer,
     autoStart: true,
     delay: 40,
     cursor: "",
   });
 }
+function generatePoem(event) {
+  event.preventDefault();
+
+  let userInstructions = document.querySelector("#user-instructions");
+  let apiKey = "080cc4c3o478b5af3td4bb7f689bee06";
+  let context =
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'Valencia AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `Write a short 4 lineromantic English poem about ${userInstructions.value} and each line should translate to chinese and display below the English line. `;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(
+    prompt
+  )}&context=${encodeURIComponent(context)}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a bilingual 4-line poem about ${userInstructions.value} </div>`;
+  poemElement.classList.remove("hidden");
+
+  axios.get(apiUrl).then((response) => {
+    console.log("API Response:", response);
+    displayPoem(response);
+  });
+}
+
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
